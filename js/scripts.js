be456af84c4fe269e8cb9f11e57971c3f368d3f5
Javier <javi086mx@gmail.com>
2026-03-28 10:32:35 -0400

    removing files

--- js/scripts.js
+++ js/scripts.js
@@ -79,23 +79,3 @@ async function getProductInformation() {
     }
 }
 
-// TO-DO Indentify fields in form and then remove the hardcode to get information from server
-
-
-
-// Function to show the modal with the correct data
-function selectPlan(id, name, price) {
-    document.getElementById('checkout-modal').classList.remove('hidden');
-
-    // Fill in the Transaction info
-    document.getElementById('modal-plan-name').innerText = name;
-    document.getElementById('modal-plan-price').innerText = `$${price} / month`;
-    document.getElementById('form-product-id').value = id;
-}
-
-function closeModal() {
-    document.getElementById('checkout-modal').classList.add('hidden');
-}
-
-// Inside your products.forEach loop, update the button to:
-// <button onclick="selectPlan('${product.id}', '${product.name}', '3.99')" ...>
